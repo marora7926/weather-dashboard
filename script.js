502c69492efc7caeaaf66684eebebf521c186218
Mohit Arora <mohit.arora@sydney.edu.au>
2021-04-02 03:21:40 +1100

    added jumbotron and updated js and css files

---
+++ script.js
@@ -0,0 +1,13 @@
+// LIVE DATE AND TIME DISPLAY ELEMENT
+// reference to important DOM custom elements
+var timeDisplayEl = $('#currentDay');
+
+// adding the current day, date and time in the jubotron
+function displayTime (){
+    var liveDay = moment().format('dddd');
+    var liveDateTime = moment().format('MMMM Do YYYY, h:mm:ss a');
+    timeDisplayEl.text(liveDay + ", " + liveDateTime + " AEDT");
+}
+
+// updating seconds every second
+setInterval(displayTime, 1000);
